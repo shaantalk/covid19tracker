f6aba5423c45809e8e09c5d1aaea6393aac7c346
Santanu Panda <shaantalk@gmail.com>
2020-04-26 09:59:34 +0530

    Updates

---
+++ precache-manifest.dd4c6aa333105ee1e65d7e984205897f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9531f0eba286575605e488e96e60239",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "d7d233b1b6b1b48a8651",
+    "url": "/covid19tracker/static/css/main.4efd4221.chunk.css"
+  },
+  {
+    "revision": "c61598d09f198cb4a7e4",
+    "url": "/covid19tracker/static/js/2.b3d200c9.chunk.js"
+  },
+  {
+    "revision": "57d433a3cf738d4f2587da1d9252fc66",
+    "url": "/covid19tracker/static/js/2.b3d200c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d233b1b6b1b48a8651",
+    "url": "/covid19tracker/static/js/main.37efd99f.chunk.js"
+  },
+  {
+    "revision": "a2db24feb3f46cbd9e15",
+    "url": "/covid19tracker/static/js/runtime-main.07cfe111.js"
+  }
+]);
